691230f462473ed80e9a60d8eedcc8f730c789af
vladbstrv <keocer@gmail.com>
2023-09-30 16:17:38 +0300

    Initial commit

---
+++ src/main/kotlin/ru/vladbstrv/proftips/ProftipsApplication.kt
@@ -0,0 +1,11 @@
+package ru.vladbstrv.proftips
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProftipsApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProftipsApplication>(*args)
+}
